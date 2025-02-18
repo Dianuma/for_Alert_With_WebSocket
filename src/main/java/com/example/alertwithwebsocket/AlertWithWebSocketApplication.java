48a4ba1e40fde849df03039a3d9f13464459c7a8
Dianuma <dltmdgus1208@gmail.com>
2025-02-18 16:19:52 +0900

    Second Commit

---
+++ src/main/java/com/example/alertwithwebsocket/AlertWithWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.alertwithwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertWithWebSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlertWithWebSocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alertwithwebsocket/AlertWithWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alertwithwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertWithWebSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
